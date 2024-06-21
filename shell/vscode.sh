079f7d0fecbe26d3f83a95531b4b4e6b4b47b17d
Frank Ricardo Ramirez <frankjony17@gmail.com>
2024-06-21 17:42:20 +0200

    remove proxy from vscode and comment some code

--- shell/vscode.sh
+++ shell/vscode.sh
@@ -6,5 +6,4 @@ export EDITOR_NON_BLOCKING="code --new-window"
 # Automatically handle specific use-cases for vscode integrated terminal
 if [ "$TERM_PROGRAM" = "vscode" ]; then
     export ALMOSONTOP='false'
-    proxy_autoload
 fi
